07de17f9bafc95946c02d47ddb0fd439573b2f75
Karen <kjgamis@live.ca>
2018-05-01 20:11:30 -0400

    done

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,50 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_new_rocket_exists
+    skip
+    assert(@rocket.name)
+    assert(@rocket.colour)
+    refute(@rocket.flying?)
+  end
+
+  def test_rocket_is_flying
+    skip
+    @rocket.lift_off
+
+    assert(@rocket.flying?)
+  end
+
+  def test_rocket_has_landed
+    skip
+    @rocket.lift_off
+    @rocket.land
+
+    refute(@rocket.flying?)
+  end
+
+  def test_rocket_status_when_flying
+    # skip
+    @rocket.lift_off
+    actual   = @rocket.status
+    expected = "Rocket #{ @rocket.name } is flying through the sky!"
+
+    assert_equal(actual, expected)
+  end
+
+
+
+  def test_new_rocket_personalized
+    rocket = Rocket.new({ name: 'saggitarius', colour: 'red', flying: true })
+
+    actual = rocket.status
+    expected = "Rocket #{ rocket.name } is flying through the sky!"
+
+    assert_equal(actual, expected)
+  end
+
 end
